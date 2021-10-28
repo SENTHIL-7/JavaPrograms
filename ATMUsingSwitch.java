38ba2350ded9a3741dc0d16a32b70e6561d2e1f2
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-28 21:13:10 +0530

    Update ATMUsingSwitch.java

--- ATMUsingSwitch.java
+++ ATMUsingSwitch.java
@@ -1,22 +1,3 @@
-//problem
-
-//For this exercise, create a simple program that will simulate an ATM transaction:
-//
-//1. Create a String variable called command, which can hold "Withdraw" or "Deposit" commands
-//
-//2. Create an int variable called balance, which holds 1000
-//
-//3. Create another int variable called amount, which holds 100
-//
-//   If the command value is "Withdraw," then subtract the amount from balance and print out:
-//
-//      "Your balance used to be 1000, and now is 900."
-//
-//   If, instead, the command value is "Deposit," then print out:
-//
-//        "Your balance used to be 1000, and now it is 1100."
-//
-
 public class ATMUsingSwitch {
 
 	public static void main(String[] args) {
