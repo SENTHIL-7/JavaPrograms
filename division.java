17951dd4f7c29131739805399838f79f73448ffe
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-29 17:35:22 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,14 @@
+
+public class division {
+
+	public static void main(String[] args) {
+		
+		division(10,3);
+
+	}
+	public static void division(int number1,int number2) {
+		float div =(float)(number1)/(float)(number2);
+		System.out.println("Division of " +number1+ "," +number2 + " is " + div);
+	}
+
+}
