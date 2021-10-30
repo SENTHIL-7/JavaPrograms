f3ef1fb67c49bc7a0665c138facee49cb9413411
SENTHIL-7 <82882024+SENTHIL-7@users.noreply.github.com>
2021-10-30 21:03:18 +0530

    Add files via upload

---
+++ recArea.java
@@ -0,0 +1,15 @@
+
+public class recArea {
+
+	public static void main(String[] args) {
+		   System.out.println(rectArea(10,-15));
+
+	}
+	  public static double rectArea(double width,double length){
+	       
+	       double area =  Math.abs(width) * Math.abs(length);
+	       return area;
+	   }
+
+
+}
